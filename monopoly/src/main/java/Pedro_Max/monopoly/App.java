35e9a6c1ffce1a23d4d43a90b540b7a1f8b9871b
pedropfr <pedro.fraga@acad.pucrs.br>
2016-11-10 20:43:36 -0200

    Teste do Hello, world na IDE: Eclipse

---
+++ monopoly/src/main/java/Pedro_Max/monopoly/App.java
@@ -0,0 +1,13 @@
+package Pedro_Max.monopoly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
